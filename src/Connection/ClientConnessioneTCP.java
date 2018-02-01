4e6543d905ea3224f9b72f8de214d691f55ee03b
unknown <armandopalermo15@gmail.com>
2018-02-01 22:07:53 +0100

    Implementazione dei metodi avviaConnessione e scrivi Messaggio della classe ClientConnessioneTCP

--- src/Connection/ClientConnessioneTCP.java
+++ src/Connection/ClientConnessioneTCP.java
@@ -5,7 +5,16 @@
  */
 package Connection;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ConnectException;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -26,12 +35,47 @@ public class ClientConnessioneTCP {
            
             Socket connection = null;
             String serverAddress = "localhost";
-            boolean a=true;
+            
+             try{
+
+                connection = new Socket(serverAddress, porta);
+                System.out.println("Connessione aperta");
+                this.scriviMessaggio(connection);
+               
+            }
+            catch(ConnectException e){
+                System.err.println("Server non disponibile!");
+            }
+            catch(UnknownHostException e1){
+                System.err.println("Errore DNS!");
+            }
+            catch(IOException e2){
+                 System.err.println("Errore I/O");
+             }
        }
+       
+       
        //scrittura messaggio da inoltrare al server
-        void scriviMessaggio(){
-        
+        void scriviMessaggio(Socket connection){
+            String messaggio="";
+            try {
+                BufferedReader inputClient= new BufferedReader(new InputStreamReader(System.in));//Input da tastiera
+                BufferedReader inputClientRispServer= new BufferedReader(new InputStreamReader(connection.getInputStream()));//Stream per gestione della risposta del server
+                PrintStream outputClient= new PrintStream(connection.getOutputStream());
+                while(!"chiudi".equals(messaggio)){
+                    System.out.println("Scrivi un messaggio da inoltrare al server");
+                    messaggio=inputClient.readLine();
+                    outputClient.println(messaggio);
+                    outputClient.flush();
+                    String rispostaServer=inputClientRispServer.readLine();
+                    System.out.println(rispostaServer);//stampo la risposta del server
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(ClientConnessioneTCP.class.getName()).log(Level.SEVERE, null, ex);
+            }
+           
         }
+        
         //chiusura della connessione in seguito all'invio del messaggio "chiudi"
         void chiudiConnessione(){
         
